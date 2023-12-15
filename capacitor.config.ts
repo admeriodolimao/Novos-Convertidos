5a2f2b0cf15cbe2deeb02a0743c91a3172b1c74c
Demetrius Ferreira <demithehomie@gmail.com>
2023-12-15 17:11:07 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nc-fe-ionic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
